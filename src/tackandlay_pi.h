b279e3b842374b8217abebf89580e825650a77f0
Rick Gleason <fcgleason@tgp-architects.com>
2016-03-24 09:17:04 -0400

    Update tackandlay_pi.h

--- src/tackandlay_pi.h
+++ src/tackandlay_pi.h
@@ -31,7 +31,6 @@
 #include "version.h"
 #include "wx/wx.h"
 #include <wx/glcanvas.h>
-//For Windows Compile comment out "//#include "oopndc.h"" in line below
 #include "ocpndc.h"
 
 #ifndef  WX_PRECOMP
